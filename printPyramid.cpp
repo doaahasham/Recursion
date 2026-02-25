c844ad0fee3e5ddb053c43cd1c5cd29bcee50c50
doaahasham <doohesham452@gmail.com>
2026-02-25 09:41:57 +0200

    print Pyamid

---
+++ printPyramid.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void printSpaces(int N) {
+    if (N == 0) return;
+    cout << " ";
+    printSpaces(N - 1);
+}
+void printStars(int N) {
+    if (N == 0) return;
+    cout << "*";
+    printStars(N - 1);
+}
+void printPyramid(int N, int rows = 1) {
+    if (rows > N) return;
+
+    printSpaces(N - rows);
+    printStars(2 * rows - 1);
+    cout << endl;
+
+    printPyramid(N, rows + 1);
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    printPyramid(N);
+
+    return 0;
+}
