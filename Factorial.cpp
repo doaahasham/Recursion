08a3680705e9b9e3efba394aef20ab879a4392fc
doaahasham <doohesham452@gmail.com>
2026-02-27 05:25:10 +0200

    factorial

---
+++ Factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void Factorial(long long N , long long sum){
+    if(N==0 || N==1)
+    {
+        cout<<sum;
+        return ;
+    }
+    Factorial(N-1 , sum*N);
+}
+int main()
+{
+    long long N;
+    cin>>N;
+    Factorial(N,1);
+
+    return 0;
+}
